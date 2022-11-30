fbf93d14c6ed745cb3937d39e1041e4feaee31b7
xanderkoen <9006097@student.zadkine.nl>
2022-11-30 14:53:18 +0100

    Initial commit

---
+++ php/index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/output.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="bg-slate-50 p-4 flex flex-start justify-between items-center">
+    <p class="text-3xl border-solid border-r pr-4">MijnEigentaartjes</p>
+    <div class="flex flex-row w-max self-center">
+        <a href="gallerij.php"<p class="px-4 hover:underline">Gallerij</p></a>
+        <a href="contact.php" ><p class="px-4 hover:underline">Contact</p></a>
+        <a href="about.php"><p class="px-4 hover:underline">Over ons</p></a>
+    </div>
+    <div class="flex flex-row">
+        <p class="mx-4 hover:underline">login</p>
+        <p class="mx-4 hover:underline">Register</p>
+    </div>
+</div>
+</body>
+</html>
